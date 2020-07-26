6a693f6d98acd6e54d23c73985d68e18ef0f5297
HappyBoy0509 <65845452+HappyBoy0509@users.noreply.github.com>
2020-07-26 17:26:39 +0530

    Update upload_to_tg.py

--- tobrot/helper_funcs/upload_to_tg.py
+++ tobrot/helper_funcs/upload_to_tg.py
@@ -137,7 +137,7 @@ async def upload_to_tg(
 
 async def upload_to_gdrive(file_upload, message, messa_ge, g_id):
     await asyncio.sleep(EDIT_SLEEP_TIME_OUT)
-    del_it = await message.edit_text("🔊 Now Uploading to ☁️ Cloud!!!")
+    del_it = await message.edit_text("🔊 Now Uploading to Gdrive!!!")
     #subprocess.Popen(('touch', 'rclone.conf'), stdout = subprocess.PIPE)
     with open('rclone.conf', 'a', newline="\n", encoding = 'utf-8') as fole:
         fole.write("[DRIVE]\n")
